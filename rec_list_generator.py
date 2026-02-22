497e348ddd59c2a1f5747a98d75507b881a5e012
banana-bomb <whitebear-fx@foxmail.com>
2026-02-22 21:47:54 +0800

    Fix spelling errors

--- rec_list_generator.py
+++ rec_list_generator.py
@@ -2,14 +2,39 @@ from typing import Literal
 
 
 class Generator:
-    def __init__(self, syllable_set: set, left_set: set, right_set: set):
+    def __init__(self, syllable_set: set, left_set: set, right_set: set) -> None:
         self._s_set = syllable_set
         self._l_set = left_set
         self._r_set = right_set
 
-    def generate(self, mode: Literal["CVVC", "VCV"], ):
-        pass
+    def generate(
+            self, 
+            mode: Literal["CVVC", "VCV", "VCV_WIRH_VC"], 
+            bmp: int, 
+            max_length: int, 
+            sss_first: bool,
+            iter_depth: int,
+            max_redu: int
+    ) -> tuple[list[str], list[str]]:
+        map = self._crate_reclist(mode=mode, max_length=max_length, sss_first=sss_first, iter_depth=iter_depth, max_redu=max_redu)
+        self._crate_oto(bmp=bmp, audio_syllable_map=map)
 
+    def _crate_reclist(
+            self,
+            mode: Literal["CVVC", "VCV", "VCV_WIRH_VC"], 
+            max_length: int,
+            sss_first: bool,
+            iter_depth: int,
+            max_redu: int
+    ) -> dict[str, tuple[str]]:
+        pass
+        
+    def _crate_oto(
+            self,
+            audio_syllable_map: dict[str, tuple[str]],
+            bmp: int
+    ) -> list[str]:
+        pass
 
 class Transformer:
     pass
